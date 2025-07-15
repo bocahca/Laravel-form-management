757eef26ae95f153a6a55b812fa82d541785deb3
oliverleon <phyromontega@gmail.com>
2025-07-15 11:51:57 +0700

    feat: add Option and Question models with fillable attributes and relationships

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'question_id',
+        'option_text',
+        'option_value'
+    ];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+    public function options()
+    {
+        return $this->belongsToMany(Option::class, 'answer_options');
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'section_id',
+        'question_text',
+        'type',
+        'position'
+    ];
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+    public function options()
+    {
+        return $this->hasMany(Option::class);
+    }
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
