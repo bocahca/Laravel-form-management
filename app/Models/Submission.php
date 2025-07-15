22dd4c53f662bb6e93262e4e27509ae3d541b988
oliverleon <phyromontega@gmail.com>
2025-07-15 11:56:20 +0700

    feat: implement Submission model with fillable attributes and relationships to Form, User, and Answer

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -2,9 +2,34 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'form_id',
+        'user_id',
+        'status',
+        'approved_by',
+        'approved_at',
+    ];
+    public function form()
+    {
+        return $this->belongsTo(Form::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
