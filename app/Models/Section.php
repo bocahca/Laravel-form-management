622510146b5a34f9b7ab52e2f011cd7254324e83
oliverleon <phyromontega@gmail.com>
2025-07-15 11:45:38 +0700

    feat: add Section model with fillable attributes and relationships to Form and Question

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Section extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'form_id',
+        'position'
+    ];
+    public function form()
+    {
+        return $this->belongsTo(Form::class);
+    }
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
