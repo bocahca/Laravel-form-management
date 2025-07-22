5095ef5ce5415a8524776df1bf97270856ffad19
oliverleon <phyromontega@gmail.com>
2025-07-22 13:33:11 +0700

    refactor: change is active default value

--- database/migrations/2025_07_15_040905_create_forms_table.php
+++ database/migrations/2025_07_15_040905_create_forms_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->boolean('is_active')->default(true);
+            $table->boolean('is_active')->default(false);
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
