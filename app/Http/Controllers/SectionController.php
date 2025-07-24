80c6fb3eaeae6e1f142b3fe1c4af84b08f0486ea
oliverleon <phyromontega@gmail.com>
2025-07-24 11:50:09 +0700

    feat: add CRUD methods to section controller

--- app/Http/Controllers/SectionController.php
+++ app/Http/Controllers/SectionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreSectionRequest;
+use App\Http\Requests\UpdateSectionRequest;
 use App\Models\Form;
 use App\Models\Section;
 use Illuminate\Http\RedirectResponse;
@@ -12,49 +14,58 @@ class SectionController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
-    }
+    // public function index()
+    // {
+    //     //
+    // }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Form $form)
     {
-        //
+        return view('admin.sections.create', compact('form'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreSectionRequest $request, Form $form)
     {
-        //
-    }
+        $lastPosition = $form->sections()->max('position') ?? 0;
 
+        $data = $request->validated();
+        $data['position'] = $lastPosition + 1;
+
+        $form->sections()->create($data);
+
+        return redirect()->route('admin.forms.show', $form)
+            ->with('success', 'Section berhasil ditambahkan!');
+    }
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+    // public function show(string $id)
+    // {
+    //     //
+    // }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Form $form, Section $section)
     {
-        //
+        return view('admin.sections.edit', compact('form', 'section'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateSectionRequest $request, Form $form, Section $section)
     {
-        //
+        $section->update($request->validated());
+        return redirect()->route('admin.forms.show', $form)
+            ->with('success', 'Section berhasil diupdate!');
     }
 
     public function moveUp(Form $form, Section $section): RedirectResponse
@@ -107,8 +118,9 @@ class SectionController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Form $form, Section $section)
     {
-        //
+        $section->delete();
+        return redirect()->route('admin.forms.show', $form)->with('success', 'Section dihapus!');
     }
 }
