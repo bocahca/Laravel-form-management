a2ccebbf8752692fbbd2c63727f4efd4c03bc113
oliverleon <phyromontega@gmail.com>
2025-07-30 11:29:20 +0700

    feat: add generate pdf for submission preview

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Submission;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class SubmissionController extends Controller
 {
@@ -47,5 +49,31 @@ class SubmissionController extends Controller
             'review_note' => $request->review_note,
         ]);
         return redirect()->back()->with('success', 'Submission telah direview!');
+    }
+
+    public function generatePdf(Submission $submission)
+    {
+
+        $submission->load('user', 'form', 'answers.question', 'answers.options');
+
+        $stampPath = '';
+        if ($submission->status === 'approved') {
+            $imagePath = public_path('images/stamp-approved.png');
+            if (File::exists($imagePath)) {
+                $stampPath = $imagePath;
+            }
+        }
+
+        $pdf = Pdf::loadView(
+            'admin.submissions.pdf',
+            [
+                'submission' => $submission,
+                'form' => $submission->form,
+                'stampPath' => $stampPath,
+            ]
+        )
+            ->setPaper('A4', 'portrait');;
+
+        return $pdf->stream('submission-' . $submission->id . '.pdf');
     }
 }
