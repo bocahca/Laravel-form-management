9bf53b48fce8a483262b8457cc6078b3f9f583a6
oliverleon <phyromontega@gmail.com>
2025-08-04 13:28:36 +0700

    feat: add index method for user submission, delete show user submission

--- app/Http/Controllers/UserSubmissionController.php
+++ app/Http/Controllers/UserSubmissionController.php
@@ -7,19 +7,9 @@ use Illuminate\Http\Request;
 
 class UserSubmissionController extends Controller
 {
-    public function show(Submission $submission)
-    {
-        if (!auth()->user()->role==='admin' && $submission->user_id !== auth()->id()) {
-            abort(403);
-        }   // Load relasi form, sections, questions, answers & options
-        $submission->load([
-            'form.sections.questions.options',
-            'answers.options',
-        ]);
 
-        return view('user.submissions.show', [
-            'submission' => $submission,
-            'form'       => $submission->form,
-        ]);
+    public function index()
+    {
+        return view('user.submissions.index');
     }
 }
