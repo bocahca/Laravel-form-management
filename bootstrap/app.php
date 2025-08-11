7e8b3c52ea43c0c3202d0bfe05ef23b6468506a7
oliverleon <phyromontega@gmail.com>
2025-08-11 10:40:31 +0700

    feat: add session expired exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\CheckRole;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -8,16 +9,21 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'auth'=> Authenticate::class,
-            'role'=> CheckRole::class
+            'auth' => Authenticate::class,
+            'role' => CheckRole::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->renderable(function (AuthenticationException $e, $request) {
+            if (! $request->expectsJson()) {
+                return redirect()->guest(route('welcome'))
+                    ->with('status', 'Sesi Anda telah berakhir. Silakan login kembali.');
+            }
+        });
     })->create();
