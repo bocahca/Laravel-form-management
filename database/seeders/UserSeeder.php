d97ad4fd59ee244351d2794ca67513abd49ebb93
oliverleon <phyromontega@gmail.com>
2025-08-28 09:28:16 +0700

    refactor: change user seeder to use data from .env

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,31 +1,32 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+    use App\Models\User;
+    use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use Illuminate\Database\Seeder;
+    use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    class UserSeeder extends Seeder
     {
-        User::create([
-            'name' => 'Test Admin',
-            'email' => 'admin@example.com',
-            'role' => 'admin',
-            'password' =>Hash::make('password123'),
-        ]);
-
-        User::create([
-            'name' => 'Test User',
-            'email' => 'user@example.com',
-            'role' => 'user',
-            'password' =>Hash::make('password123'),
-        ]);
+        /**
+         * Run the database seeds.
+         */
+        public function run(): void
+        {
+            $email = env('ADMIN_EMAIL');
+            $password = env('ADMIN_PASSWORD');
+            if ($email && $password) {
+                User::updateOrCreate(
+                    ['email' => $email],
+                    [
+                        'name' => 'Admin',
+                        'password' => bcrypt($password),
+                    ]
+                );
+            } else {
+                $this->command->error('Please set ADMIN_EMAIL and ADMIN_PASSWORD in your .env file.');
+                return;
+            }
+        }
     }
-}
